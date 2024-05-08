ceeff5ea300c6c7dc6ba30d262928722cfe76b5d
zibillionbytes <f.alkan.93@outlook.com>
2024-05-08 12:28:46 +0300

    first commit

---
+++ src/main/java/com/test/AccessWithJPA/AccessWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.test.AccessWithJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessWithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessWithJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/AccessWithJPA/AccessWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.AccessWithJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
