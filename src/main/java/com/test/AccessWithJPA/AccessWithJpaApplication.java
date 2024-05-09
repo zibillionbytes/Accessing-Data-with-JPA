be2f78987aff6c8f986a70c151b98233ece98a6b
zibillionbytes <f.alkan.93@outlook.com>
2024-05-09 15:17:15 +0300

    before creating executable jar

--- src/main/java/com/test/AccessWithJPA/AccessWithJpaApplication.java
+++ src/main/java/com/test/AccessWithJPA/AccessWithJpaApplication.java
@@ -1,13 +1,52 @@
 package com.test.AccessWithJPA;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccessWithJpaApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(AccessWithJpaApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(AccessWithJpaApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+			// save a few customers
+			repository.save(new Customer("Barış", "Manço"));
+			repository.save(new Customer("Cem", "Karaca"));
+			repository.save(new Customer("Erkin", "Koray"));
+			repository.save(new Customer("Ahmet", "Güvenç"));
+			repository.save(new Customer("Cahit", "Berkay"));
+
+			// fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			repository.findAll().forEach(customer -> {
+				log.info(customer.toString());
+			});
+			log.info("");
+
+			// fetch an individual customer by ID
+			Customer customer = repository.findById(1L);
+			log.info("Customer found with findById(1L):");
+			log.info("--------------------------------");
+			log.info(customer.toString());
+			log.info("");
+
+			// fetch customers by last name
+			log.info("Customer found with findByLastName('Bauer'):");
+			log.info("--------------------------------------------");
+			repository.findByLastName("Güvenç").forEach(güvenç -> {
+				log.info(güvenç.toString());
+			});
+			log.info("");
+		};
+	}
 }
---
+++ src/main/java/com/test/AccessWithJPA/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.test.AccessWithJPA;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+
+    Customer findById(long id);
+}
